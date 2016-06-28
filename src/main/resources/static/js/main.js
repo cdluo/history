3db931207a6df932c6a3e42bc85e347cd3a7a973
Chris Luo <christopher_luo@brown.edu>
2016-06-27 20:59:16 -0400

    Progress bars for social and living, background image

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -48,14 +48,15 @@ function drawWorld(){
 			//make a new canvas
 			var canv = document.createElement('canvas');
 			canv.id = curWorld[i].name;
+			canv.style.border = "1px solid #000000";
 
 			//Actual height
-			canv.width = 500;
+			canv.width = 450;
 			canv.height = 300;
 
 			//Displayed height
 			// canv.style.width = "49%";
-			// canv.style.height = "49%";
+			// canv.style.height = "75%";
 
 			document.getElementById("canvasWorld").appendChild(canv);
 			init(canv.id, curWorld[i]);		//Instantiate a stage for the new canvas.
@@ -77,63 +78,58 @@ function init(canvas, nation){
         stage.update(event);
     }
 
-  // Pop
+  //////////
+  // Name //
+  //////////
+
+	var graphics = new createjs.Graphics();
+  graphics.setStrokeStyle("0");
+  graphics.beginStroke("#000000");
+	graphics.beginFill("#9FAEB3");
+	graphics.drawRect(0,0,500,30);
+
+	var header = new createjs.Shape(graphics);
+  header.x = 0;
+  header.y = 0;
+  stage.addChild(header);
+
+  var name = new createjs.Text(nation.name, "24px Arial", "#000000");
+	name.x = 10;
+	name.y = 3;
+
+	name.name = "name";
+	stage.addChild(name);
+
+	///////////////////
+	// Progress Bars //
+	///////////////////
+
+	var socialBar = getProgressBar(stage, nation.social);
+	var socialBar1 = new createjs.Shape(socialBar);
+  socialBar1.x = 125;
+  socialBar1.y = 160;
+
+	socialBar1.name = "socialBar";
+	stage.addChild(socialBar1);
+
+	var livingBar = getProgressBar(stage, nation.living);
+	var livingBar1 = new createjs.Shape(livingBar);
+  livingBar1.x = 125;
+  livingBar1.y = 230;
+
+	livingBar1.name = "livingBar";
+	stage.addChild(livingBar1);
+
+	/////////
+  // Pop //
+  /////////
+
 	var ff6Person = new Image();
 	ff6Person.src = "images/FF6Sprite.png";
-	ff6Person.onload = addPerson(stage, ff6Person);
 
-	var popLabel = new createjs.Text(nation.pop, "24px Arial", "#000000");
-	popLabel.x = 125;
-	popLabel.y = 30;
-
-	popLabel.name = "popLabel";
-	stage.addChild(popLabel);
-
-	// Gdp
-	var dollar = new Image();
-	dollar.src = "images/dollarSpriteSheet.png";
-	dollar.onload = addDollar(stage, dollar);
-
-	var gdpLabel = new createjs.Text(nation.gdp, "24px Arial", "#000000");
-	gdpLabel.x = 125;
-	gdpLabel.y = 90;
-
-	gdpLabel.name = "gdpLabel";
-	stage.addChild(gdpLabel);
-
-	//Social
-	var social = new Image();
-	social.src = "images/Social.png";
-	social.onload =  addSocial(stage,social);
-
-	var socialLabel = new createjs.Text(nation.social, "24px Arial", "#000000");
-	socialLabel.x = 125;
-	socialLabel.y = 150;
-
-	socialLabel.name = "socialLabel";
-	stage.addChild(socialLabel);
-
-	//Living
-	var living = new Image();
-	living.src = "images/Living.png";
-	living.onload =  addLiving(stage,living);
-
-	var livingLabel = new createjs.Text(nation.living, "24px Arial", "#000000");
-	livingLabel.x = 125;
-	livingLabel.y = 230;
-
-	livingLabel.name = "livingLabel";
-	stage.addChild(livingLabel);
-}
-
-/*
-	Adds a person based on a spritesheet image for the passed in stage.
-*/
-function addPerson(stage, image){
-
-	var person = {
-	  images: [image],
-	  frames: {width:20, height:30, regX:-10, regY:0, spacing:0, margin:0},
+	var data = {
+	  images: [ff6Person],
+	  frames: {width:20, height:30, regX:-17, regY:-17, spacing:0, margin:0},
 	  animations: {
 	  	one: [0],	
 	  	two: [1],
@@ -142,49 +138,49 @@ function addPerson(stage, image){
     }
   };
 
-  var spriteSheet = new createjs.SpriteSheet(person);
+  ff6Person.onload = addSprite(stage, data, 2.25, 2.25, "walk", "person");
 
-  var sprite = new createjs.Sprite(spriteSheet);
-  sprite.scaleX = 2.5;
-  sprite.scaleY = 2.5;
-  sprite.gotoAndPlay("walk");
+	var popLabel = new createjs.Text(nation.pop, "24px Arial", "#000000");
+	popLabel.x = 125;
+	popLabel.y = 55;
 
-  sprite.name = "person";
-  stage.addChild(sprite);
-}
+	popLabel.name = "popLabel";
+	stage.addChild(popLabel);
 
-/*
-	Adds a dollar sprite
-*/
-function addDollar(stage, image){
+	/////////
+	// Gdp //
+	/////////
 
-	var data = {
-	  images: [image],
-	  frames: {width:50, height:20, regX:-3, regY:-50, spacing:5, margin:0},
+	var dollar = new Image();
+	dollar.src = "images/dollarSpriteSheet.png";
+
+	data = {
+	  images: [dollar],
+	  frames: {width:50, height:20, regX:-10, regY:-60, spacing:5, margin:0},
 	  animations: {
 	    shine: [0,2,"shine",0.2]
     }
   };
 
-  var spriteSheet = new createjs.SpriteSheet(data);
+	dollar.onload = addSprite(stage, data, 1.75, 1.75, "shine", "dollar");
 
-  var sprite = new createjs.Sprite(spriteSheet);
-  sprite.scaleX = 1.75;
-  sprite.scaleY = 1.75;
-  sprite.gotoAndPlay("shine");
+	var gdpLabel = new createjs.Text(nation.gdp, "24px Arial", "#000000");
+	gdpLabel.x = 125;
+	gdpLabel.y = 105;
 
-  sprite.name = "dollar";
-  stage.addChild(sprite);
-}
+	gdpLabel.name = "gdpLabel";
+	stage.addChild(gdpLabel);
 
-/*
-	Adds a speech bubble
-*/
-function addSocial(stage, image){
+	////////////
+	// Social //
+	////////////
 
-	var data = {
-	  images: [image],
-	  frames: {width:71, height:61, regX:0, regY:-100, spacing:0, margin:0},
+	var social = new Image();
+	social.src = "images/Social.png";
+
+	data = {
+	  images: [social],
+	  frames: {width:71, height:61, regX:-20, regY:-145, spacing:0, margin:0},
 	  animations: {
 	  	heart: [8,9,"heart",0.1],	
 	  	dot: {
@@ -200,25 +196,25 @@ function addSocial(stage, image){
     }
   };
 
-  var spriteSheet = new createjs.SpriteSheet(data);
+	social.onload =  addSprite(stage, data, 1, 1, "heart", "social");
 
-  var sprite = new createjs.Sprite(spriteSheet);
-  sprite.scaleX = 1.25;
-  sprite.scaleY = 1.25;
-  sprite.gotoAndPlay("heart");
+	// var socialLabel = new createjs.Text(nation.social, "24px Arial", "#000000");
+	// socialLabel.x = 125;
+	// socialLabel.y = 150;
 
-  sprite.name = "social";
-  stage.addChild(sprite);
-}
+	// socialLabel.name = "socialLabel";
+	// stage.addChild(socialLabel);
 
-/*
-	Adds a Tree.
-*/
-function addLiving(stage, image){
+	////////////
+	// Living //
+	////////////
 
-	var data = {
-	  images: [image],
-	  frames: {width:96, height:96, regX:-3, regY:-200, spacing:0, margin:0},
+	var living = new Image();
+	living.src = "images/Living.png";
+
+	data = {
+	  images: [living],
+	  frames: {width:96, height:96, regX:-28, regY:-265, spacing:0, margin:0},
 	  animations: {
 	  	spring: [0,2,"spring",0.05],	
 	  	fall: [3,4,"fall",0.05],	
@@ -226,17 +222,51 @@ function addLiving(stage, image){
     }
   };
 
-  var spriteSheet = new createjs.SpriteSheet(data);
+	living.onload =  addSprite(stage, data, 0.8, 0.8, "spring", "living");
 
-  var sprite = new createjs.Sprite(spriteSheet);
-  sprite.scaleX = 1;
-  sprite.scaleY = 1;
-  sprite.gotoAndPlay("spring");
+	// var livingLabel = new createjs.Text(nation.living, "24px Arial", "#000000");
+	// livingLabel.x = 125;
+	// livingLabel.y = 230;
 
-  sprite.name = "living";
-  stage.addChild(sprite);
+	// livingLabel.name = "livingLabel";
+	// stage.addChild(livingLabel);
 }
 
+/*
+	Creates a progressBar graphics instance that can be added through
+	the Shape class.
+
+	@param percent: how full this progress bar should appear.
+*/
+function getProgressBar(stage, percent){
+	var graphics = new createjs.Graphics();
+	graphics.setStrokeStyle(1);
+
+	graphics.beginStroke("#000000");
+	graphics.beginFill("white");
+	graphics.drawRoundRect(0,0,300,30,10);
+	graphics.endStroke();
+	graphics.endFill;
+
+	var greenLength = 3 * percent;
+	graphics.beginStroke("#000000");
+	graphics.beginFill("green");
+	graphics.drawRoundRect(0,0,greenLength,30,10);
+
+	return graphics;
+}
+
+function addSprite(stage, data, scaleX, scaleY, startAni, name){
+	var spriteSheet = new createjs.SpriteSheet(data);
+
+  var sprite = new createjs.Sprite(spriteSheet);
+  sprite.scaleX = scaleX;
+  sprite.scaleY = scaleY;
+  sprite.gotoAndPlay(startAni);
+
+  sprite.name = name;
+  stage.addChild(sprite);
+}
 
 /*
 	Draws a nation as part of drawWorld().
@@ -244,7 +274,72 @@ function addLiving(stage, image){
 function drawNation(nation){
 	var stage = canvStgMap[nation.name];
 
-	//Use for updating the stuff you instantiated in init.
+	// Progress Bars
+	stage.removeChild(stage.getChildByName("socialBar"));
+
+	var socialBar = getProgressBar(stage, nation.social);
+	var socialBar1 = new createjs.Shape(socialBar);
+  socialBar1.x = 125;
+  socialBar1.y = 160;
+
+	socialBar1.name = "socialBar";
+	stage.addChild(socialBar1);
+	// stage.setChildIndex(socialBar1, 0);		//Sends to back, only if you want numbers.
+
+	stage.removeChild(stage.getChildByName("livingBar"));
+
+	var livingBar = getProgressBar(stage, nation.living);
+	var livingBar1 = new createjs.Shape(livingBar);
+  livingBar1.x = 125;
+  livingBar1.y = 230;
+
+	livingBar1.name = "livingBar";
+	stage.addChild(livingBar1);
+	// stage.setChildIndex(livingBar1, 0);		//Sends to back, only if you want numbers.
+
+	//Labels
+	var popLabel = stage.getChildByName("popLabel");
+	popLabel.text = nation.pop;
+
+	var gdpLabel = stage.getChildByName("gdpLabel");
+	gdpLabel.text = nation.gdp;
+
+	// var socialLabel = stage.getChildByName("socialLabel");
+	// socialLabel.text = nation.social;
+
+	// var livingLabel = stage.getChildByName("livingLabel");
+	// livingLabel.text = nation.living;
+
+	//Animations
+	var bubble = stage.getChildByName("social");
+	if(nation.social > 66){
+		if(bubble.currentAnimation != "heart"){
+			bubble.gotoAndPlay("heart");
+		}
+	}else if(nation.social > 33){
+		if(bubble.currentAnimation != "dot"){
+			bubble.gotoAndPlay("dot");
+		}
+	}else{
+		if(bubble.currentAnimation != "anger"){
+			bubble.gotoAndPlay("anger");
+		}
+	}
+
+	var tree = stage.getChildByName("living");
+	if(nation.living > 66){
+		if(tree.currentAnimation != "spring"){
+			tree.gotoAndPlay("spring");
+		}
+	}else if(nation.living > 33){
+		if(tree.currentAnimation != "fall"){
+			tree.gotoAndPlay("fall");
+		}
+	}else{
+		if(tree.currentAnimation != "winter"){
+			tree.gotoAndPlay("winter");
+		}
+	}
 }
 
 /**
